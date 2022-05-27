a58a6d1c03acfed5d92b5ea55a1e2e14fec76b81
ahmed_mohamed__ <ahmed.mohamed.fci@gmail.com>
2022-05-27 17:56:05 +0200

    changed the look of buy-sell-select inputs

--- src/components/right-panel/right-panel.js
+++ src/components/right-panel/right-panel.js
@@ -1,11 +1,28 @@
 import './right-panel.css';
 import Chart from './chart/chart';
+import { useState } from 'react';
 
 function RightPanel(props) {
+    let [selectedLot, setSelectedLot] = useState(1000);
+    function selectedChanged(event){
+       setSelectedLot(event.target.value);
+    }
     return (
     <div className='right-div'>
+          <div className='buy-sell-div'>
+            <div>
+              <button className='buy-button'>buy</button>
+              <button className='sell-button'>sell</button>
+            </div>
+            <select onChange={selectedChanged}>
+              {Array(10).fill(0).map((x,i)=> (1000 * (i+1))).map(lotSize => <option value={lotSize}>{lotSize} {props.baseCurrency}</option>)}
+            </select>
+            
+          </div>
+          
           <Chart
             baseCurrency={props.baseCurrency} symbolCurrency={props.symbolCurrency}/>
+
           <div className='border'>
               logger
           </div>
